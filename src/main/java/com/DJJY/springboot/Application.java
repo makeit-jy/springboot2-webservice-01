9da1fb69f247de0556ef1089d6c0834ba37bffe9
makeit-jy <makeit_jy@naver.com>
2020-08-03 16:09:05 +0900

    commit

---
+++ src/main/java/com/DJJY/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.DJJY.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
