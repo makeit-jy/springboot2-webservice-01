dcaec5009acc611374da89e4a670eeebf61e5010
makeit-jy <makeit_jy@naver.com>
2020-08-06 22:22:55 +0900

    Google Login

--- src/main/java/com/DJJY/springboot/web/IndexController.java
+++ src/main/java/com/DJJY/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.DJJY.springboot.web;
 
+import com.DJJY.springboot.config.auth.dto.SessionUser;
 import com.DJJY.springboot.service.posts.PostsService;
 import com.DJJY.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("memberName", user.getName());
+        }
         return "index";
     }
 
