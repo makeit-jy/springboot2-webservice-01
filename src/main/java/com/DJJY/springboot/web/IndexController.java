7c0a962bd64becfd81de8f0ee5ca789248bb174f
makeit-jy <makeit_jy@naver.com>
2020-08-07 20:52:52 +0900

    Google login update

--- src/main/java/com/DJJY/springboot/web/IndexController.java
+++ src/main/java/com/DJJY/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.DJJY.springboot.web;
 
+import com.DJJY.springboot.config.auth.LoginUser;
 import com.DJJY.springboot.config.auth.dto.SessionUser;
 import com.DJJY.springboot.service.posts.PostsService;
 import com.DJJY.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("memberName", user.getName());
         }
